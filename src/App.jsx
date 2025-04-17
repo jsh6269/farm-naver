dc8662e987fe9f5a0723cd06ae6013451e8c38a9
suhan8984 <suhan8984@daum.net>
2025-04-18 01:51:30 +0900

    feat: dummy pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import bgImage from "./assets/common/bg.png";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import PointShopPage from "./routes/PointShopPage";
+import ItemShopPage from "./routes/ItemShopPage";
+import SalePage from "./routes/SalePage";
+import PetShopPage from "./routes/PetShopPage";
+
+import CrystalPage from "./routes/CrystalPage";
+import ComicPage from "./routes/ComicPage";
 import FestivalPage from "./routes/FestivalPage";
+import FoodShopPage from "./routes/FoodShopPage";
+import MixDogPage from "./routes/MixDogPage";
+import MerchantPage from "./routes/MerchantPage";
+import HospitalPage from "./routes/HospitalPage";
+import SchoolPage from "./routes/SchoolPage";
+import PostPage from "./routes/PostPage";
+import ExplorePage from "./routes/ExplorePage";
+
 import { useEffect } from "react";
 
 const ScrollToTop = () => {
@@ -37,7 +53,24 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+
+          {/* 상단 메뉴 */}
+          <Route path="/pointshop" element={<PointShopPage />} />
+          <Route path="/itemshop" element={<ItemShopPage />} />
+          <Route path="/sale" element={<SalePage />} />
+          <Route path="/petshop" element={<PetShopPage />} />
+
+          {/* 하단 메뉴 */}
+          <Route path="/crystal" element={<CrystalPage />} />
+          <Route path="/comic" element={<ComicPage />} />
           <Route path="/festival" element={<FestivalPage />} />
+          <Route path="/foodshop" element={<FoodShopPage />} />
+          <Route path="/mix_dog" element={<MixDogPage />} />
+          <Route path="/merchant" element={<MerchantPage />} />
+          <Route path="/hospital" element={<HospitalPage />} />
+          <Route path="/school" element={<SchoolPage />} />
+          <Route path="/post" element={<PostPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/routes/FoodShopPage.jsx
@@ -0,0 +1,46 @@
+import Sidebar from "../components/Sidebar";
+import festivalHeader from "../assets/festival/img_festival01.gif";
+import btn1 from "../assets/festival/btn_festival28_ani_album.gif";
+import btn2 from "../assets/festival/btn_festival28_ani_festival.gif";
+import btn3 from "../assets/festival/btn_festival28_farm_festiva.gif";
+
+const FoodShopPage = () => {
+  return (
+    <div className="bg-[#FFF799] rounded-2xl flex">
+      <div className="text-center mt-1" style={{ fontFamily: "굴림" }}>
+        <span className="text-[12px]">
+          <a href="/" className="text-blue-700">
+            동물농장
+          </a>
+          &gt; 페스티벌
+          <Sidebar />
+        </span>
+      </div>
+      <div className="w-[550px] h-[738px] bg-white rounded-2xl mt-[33px] mr-5">
+        <img src={festivalHeader} />
+        <div className="bg-gradient-to-b from-amber-200 to-[#FEFBD9] mx-[10px] h-[470px] rounded-b-xl">
+          <div
+            className="bg-white mx-[10px] rounded-b-[5.5px] h-[459px] pt-4 px-5"
+            style={{ fontFamily: "굴림", fontSize: "12px" }}
+          >
+            <h3 className="font-semibold">
+              이번주 페스티발 주제는 “으스스 할로윈파티” 입니다.
+            </h3>
+            <p>
+              좀 있으면 다가오는 할로윈 데이를 기념하여 동물농장에서도
+              으스스하고 재미있는 할로윈 파티가 열렸나 봐요~!
+            </p>
+            <div className="flex flex-start flex-wrap gap-x-10.5 pl-1.5 gap-y-1 pt-5">
+              <img src={btn2} />
+              <img src={btn3} />
+            </div>
+            <div className="pt-3">
+              <img src={btn1} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FoodShopPage;
