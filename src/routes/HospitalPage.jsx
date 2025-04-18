7f83f97183d86c83f3eeb39d5df798c53fffbc76
suhan8984 <suhan8984@daum.net>
2025-04-18 12:27:05 +0900

    feat: hospital page

--- src/routes/HospitalPage.jsx
+++ src/routes/HospitalPage.jsx
@@ -1,8 +1,9 @@
 import Sidebar from "../components/Sidebar";
-import festivalHeader from "../assets/festival/img_festival01.gif";
-import btn1 from "../assets/festival/btn_festival28_ani_album.gif";
-import btn2 from "../assets/festival/btn_festival28_ani_festival.gif";
-import btn3 from "../assets/festival/btn_festival28_farm_festiva.gif";
+import hospitalHeader from "../assets/hospital/img_hospital10.gif";
+import marsh from "../assets/hospital/marsh02s.gif";
+import btn1 from "../assets/hospital/btn_hospital01.gif";
+import btn2 from "../assets/hospital/btn_hospital02.gif";
+import btn3 from "../assets/hospital/btn_hospital03.gif";
 
 const HospitalPage = () => {
   return (
@@ -16,26 +17,26 @@ const HospitalPage = () => {
           <Sidebar />
         </span>
       </div>
-      <div className="w-[550px] h-[738px] bg-white rounded-2xl mt-[33px] mr-5">
-        <img src={festivalHeader} />
-        <div className="bg-gradient-to-b from-amber-200 to-[#FEFBD9] mx-[10px] h-[470px] rounded-b-xl">
+      <div className="w-[550px] h-[708px] bg-white rounded-2xl mt-[33px] mr-5">
+        <img src={hospitalHeader} />
+        <div className="bg-gradient-to-b from-[#77df06] to-[#e5f7fc] mx-[10px] h-[440px] rounded-b-xl">
           <div
-            className="bg-white mx-[10px] rounded-b-[5.5px] h-[459px] pt-4 px-5"
+            className="bg-white mx-[10px] rounded-b-[5.5px] h-[429px] pt-4 px-5"
             style={{ fontFamily: "굴림", fontSize: "12px" }}
           >
-            <h3 className="font-semibold">
-              이번주 페스티발 주제는 “으스스 할로윈파티” 입니다.
-            </h3>
-            <p>
-              좀 있으면 다가오는 할로윈 데이를 기념하여 동물농장에서도
-              으스스하고 재미있는 할로윈 파티가 열렸나 봐요~!
-            </p>
-            <div className="flex flex-start flex-wrap gap-x-10.5 pl-1.5 gap-y-1 pt-5">
-              <img src={btn2} />
-              <img src={btn3} />
+            <div className="flex justify-center gap-5 items-center mb-3 mt-1">
+              <img src={marsh} />
+              <p className="text-[13.3px]">
+                이곳은 동물 친구들의 건강뿐만 아니라 <br />
+                마음의 병까지 고쳐드리는 곳이랍니다.
+              </p>
             </div>
-            <div className="pt-3">
+            <div className="flex flex-start flex-wrap gap-x-10.5 ml-13 gap-y-1 pt-5">
               <img src={btn1} />
+              <img src={btn2} />
+            </div>
+            <div className="pt-6 ml-13">
+              <img src={btn3} />
             </div>
           </div>
         </div>
