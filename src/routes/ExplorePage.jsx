b2a0ca33d1456d1d375aedfb1e4e78c0c873d0f9
suhan8984 <suhan8984@daum.net>
2025-04-18 23:09:43 +0900

    feat: explore page

--- src/routes/ExplorePage.jsx
+++ src/routes/ExplorePage.jsx
@@ -1,13 +1,78 @@
 import Sidebar from "../components/Sidebar";
-import festivalHeader from "../assets/festival/img_festival01.gif";
-import btn1 from "../assets/festival/btn_festival28_ani_album.gif";
-import btn2 from "../assets/festival/btn_festival28_ani_festival.gif";
-import btn3 from "../assets/festival/btn_festival28_farm_festiva.gif";
+import img01 from "../assets/explore/top_0924_img01.gif";
+import img02 from "../assets/explore/top_0924_img02.gif";
+import img03 from "../assets/explore/top_0924_img03.gif";
+import img04 from "../assets/explore/top_0924_img04.gif";
+import img05 from "../assets/explore/top_0924_img05.gif";
+import img06 from "../assets/explore/top_0924_img06.gif";
+import img07 from "../assets/explore/top_0924_img07.gif";
+import img08 from "../assets/explore/top_0924_img08.gif";
+import img09 from "../assets/explore/top_0924_img09.gif";
+import img10 from "../assets/explore/top_0924_img10.gif";
+
+import btn01 from "../assets/explore/btn_0924_01.gif";
+import btn02 from "../assets/explore/btn_0924_02.gif";
+import btn03 from "../assets/explore/btn_0924_03.gif";
+import btn04 from "../assets/explore/btn_0924_04.gif";
+
+const imageRows = [
+  [img01, img02],
+  [img03, img04],
+  [img05, img06],
+  [img07, img08],
+  [img09, img10],
+];
+
+const ExploreLayout = () => (
+  <div
+    className="rounded-b-[13px] bg-[#6C32E0] box-border p-0 m-0"
+    style={{ width: 555 }}
+  >
+    {/* 이미지 영역 */}
+    <div>
+      {imageRows.map((row, i) => (
+        <div key={i} className="flex m-0 p-0 leading-none">
+          {row.map((src, j) => (
+            <img
+              src={src}
+              alt={`top_0924_img${String(i * 2 + j + 1).padStart(2, "0")}`}
+              key={src}
+              className="block m-0 p-0 border-0 select-none"
+              draggable={false}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+
+    {/* 버튼 영역 */}
+    <div className="w-[555px] m-0 p-0">
+      <div className="flex justify-center m-0 p-0">
+        <a href="#" tabIndex={-1} className="outline-none">
+          <img src={btn01} alt="btn_0924_01" className="block border-0" />
+        </a>
+        <a href="#" tabIndex={-1} className="outline-none">
+          <img src={btn02} alt="btn_0924_02" className="block border-0" />
+        </a>
+      </div>
+      <div className="h-[20px]" />
+      <div className="flex justify-center m-0 p-0">
+        <a href="#" tabIndex={-1} className="outline-none">
+          <img src={btn03} alt="btn_0924_03" className="block border-0" />
+        </a>
+        <a href="#" tabIndex={-1} className="outline-none">
+          <img src={btn04} alt="btn_0924_04" className="block border-0" />
+        </a>
+      </div>
+      <div className="h-[18px]" />
+    </div>
+  </div>
+);
 
 const ExplorePage = () => {
   return (
     <div className="bg-[#9966ff] rounded-2xl flex">
-      <div className="mt-1" style={{ fontFamily: "굴림" }}>
+      <div className="mt-1 font-gulim">
         <span className="text-[12px] text-start ml-5">
           <a href="/" className="text-blue-700">
             동물농장
@@ -16,29 +81,8 @@ const ExplorePage = () => {
           <Sidebar />
         </span>
       </div>
-      <div className="w-[550px] h-[738px] bg-white rounded-2xl mt-[33px] mr-5">
-        <img src={festivalHeader} />
-        <div className="bg-gradient-to-b from-amber-200 to-[#FEFBD9] mx-[10px] h-[470px] rounded-b-xl">
-          <div
-            className="bg-white mx-[10px] rounded-b-[5.5px] h-[459px] pt-4 px-5"
-            style={{ fontFamily: "굴림", fontSize: "12px" }}
-          >
-            <h3 className="font-semibold">
-              이번주 페스티발 주제는 “으스스 할로윈파티” 입니다.
-            </h3>
-            <p>
-              좀 있으면 다가오는 할로윈 데이를 기념하여 동물농장에서도
-              으스스하고 재미있는 할로윈 파티가 열렸나 봐요~!
-            </p>
-            <div className="flex flex-start flex-wrap gap-x-10.5 pl-1.5 gap-y-1 pt-5">
-              <img src={btn2} />
-              <img src={btn3} />
-            </div>
-            <div className="pt-3">
-              <img src={btn1} />
-            </div>
-          </div>
-        </div>
+      <div className="w-[550px] h-[738px] mt-[33px] mr-5">
+        <ExploreLayout />
       </div>
     </div>
   );
