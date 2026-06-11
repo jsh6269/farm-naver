b6563aa41f2f7805296b0364091d436766602494
suhan8984 <suhan8984@daum.net>
2026-06-12 01:00:35 +0900

    feat: comics

--- src/routes/ComicPage.jsx
+++ src/routes/ComicPage.jsx
@@ -11,18 +11,22 @@ import comicb2 from "../assets/comic/farm_comic_cardbattle02.jpg";
 import comicb3 from "../assets/comic/farm_comic_cardbattle03.jpg";
 import comicb4 from "../assets/comic/farm_comic_cardbattle04.jpg";
 import comicb5 from "../assets/comic/farm_comic_cardbattle05.jpg";
+import comici1 from "../assets/comic/img_book2_1.jpg";
+import comici2 from "../assets/comic/img_book2_2.jpg";
 
 const comicPairs = [
-  { img: comic1, caption: "동물농장 1권" },
-  { img: comic2, caption: "동물농장 2권" },
-  { img: comic3, caption: "동물농장 3권" },
-  { img: comic4, caption: "동물농장 4권" },
-  { img: comic5, caption: "동물농장 5권" },
-  { img: comicb1, caption: "카드배틀 1권" },
-  { img: comicb2, caption: "카드배틀 2권" },
-  { img: comicb3, caption: "카드배틀 3권" },
-  { img: comicb4, caption: "카드배틀 4권" },
-  { img: comicb5, caption: "카드배틀 5권" },
+  { img: comic1, caption: "동물농장 1권", url: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=687408" },
+  { img: comic2, caption: "동물농장 2권", url: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=830382" },
+  { img: comic3, caption: "동물농장 3권", url: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=873013" },
+  { img: comic4, caption: "동물농장 4권", url: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=890153" },
+  { img: comic5, caption: "동물농장 5권", url: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=914718" },
+  { img: comicb1, caption: "카드배틀 1권", url: "https://search.shopping.naver.com/book/catalog/53915753949" },
+  { img: comicb2, caption: "카드배틀 2권", url: "https://search.shopping.naver.com/book/catalog/53911312742" },
+  { img: comicb3, caption: "카드배틀 3권", url: "https://search.shopping.naver.com/book/catalog/53915618344" },
+  { img: comicb4, caption: "카드배틀 4권", url: "https://search.shopping.naver.com/book/catalog/53911300958" },
+  { img: comicb5, caption: "카드배틀 5권", url: "https://search.shopping.naver.com/book/catalog/53915512935" },
+  { img: comici1, caption: "종이모형 1권", url: "https://www.coupang.com/vp/products/8179356689" },
+  { img: comici2, caption: "종이모형 2권", url: "https://www.coupang.com/vp/products/8179356609" },
 ];
 
 const ComicPage = () => {
@@ -41,21 +45,30 @@ const ComicPage = () => {
     >
         <img src={comicHeader} className="mt-4 ml-2" alt="코믹북 헤더" />
         <div className="h-[459px] pt-4 px-5 font-gulim text-[12px]">
-          <img
-            src={comicSub}
-            className="mx-auto mt-1 mb-2"
-            alt="동물농장 관련 책 모두보기"
-          />
+          <a
+            href="https://farm.jr.naver.com/comic/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block mx-auto mt-1 mb-2 w-fit"
+          >
+            <img
+              src={comicSub}
+              alt="동물농장 관련 책 모두보기"
+            />
+          </a>
           <div className="mx-[33px]">
             <div className="flex flex-wrap gap-x-12 pl-1.5 gap-y-1 pt-5">
-              {comicPairs.map(({ img, caption }, idx) => (
-                <div
-                  key={idx}
-                  className="text-center text-[14px] text-blue-700"
+              {comicPairs.map(({ img, caption, url }) => (
+                <a
+                  key={caption}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block text-center text-[14px] text-blue-700 cursor-pointer hover:underline"
                 >
                   <img src={img} alt={caption} />
                   <p className="mt-1 mb-3">{caption}</p>
-                </div>
+                </a>
               ))}
             </div>
           </div>
